b8373eb136872e5c75f94b1e7394851ec68c0d83
aaryadewangga <arya.angga45@gmail.com>
2022-03-29 04:21:35 +0700

    update html template

--- app/routes.go
+++ app/routes.go
@@ -2,6 +2,7 @@ package app
 
 import (
 	"P2/app/controllers"
+	"net/http"
 
 	"github.com/gorilla/mux"
 )
@@ -10,4 +11,7 @@ func (server *Server) InitializeRoute() {
 	server.Router = mux.NewRouter()
 	server.Router.HandleFunc("/", controllers.Home).Methods("GET")
 
+	staticFileDirectory := http.Dir("./assets/")
+	staticFileHandler := http.StripPrefix("/public/", http.FileServer(staticFileDirectory))
+	server.Router.PathPrefix("/public/").Handler(staticFileHandler).Methods("GET")
 }
